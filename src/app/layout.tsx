20ae5fbd011d73de9c53195b6be4d860773ac221
3-201412419 <dok2andtablo@naver.com>
2024-09-02 18:54:39 +0900

    oct_next_app_basic

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <p>현대카드 무이자 이벤트 중</p>
+        {children}</body>
     </html>
   );
 }
