dcffb4ef2600c9bbb09bb222480b1fb42d6d946d
3-201412419 <dok2andtablo@naver.com>
2024-09-03 14:29:16 +0900

    test_commit

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,17 +1,32 @@
+import Comment from './datas';
+
 export default function Cart() {
+    let food : any = ['Tomatoes', 'Pastas'];
     return (
       <div>
         <h4 className="title">Cart</h4>
-        <div className="cart-item">
-          <p>상품명</p>
-          <p>$40</p>
-          <p>1개</p>
-        </div>
-        <div className="cart-item">
-          <p>상품명</p>
-          <p>$40</p>
-          <p>1개</p>
-        </div>
+        <Comment />
+        <Cartitem item = {food[0]}/>
+        <Cartitem item = {food[1]}/>
+        <Button item = "red" />
+        <Button item = "blue" />
       </div>
     )
   } 
+
+  function Button(props : any){
+    return (
+        <button style = {{background : `${props.item}`, margin : '50px'}}>123</button>
+    )
+  }
+
+
+  function Cartitem(props : any){
+    return (
+        <div className="cart-item">
+        <p>{props.item}</p>
+        <p>$40</p>
+        <p>1개</p>
+      </div>
+    )
+    }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
+'use client';
 
+import { useState } from "react";
 
 export default function Home() {
 
   let goods = ['Tomatoes', 'Pasta', 'Coconut']
   let prices = [40,60,80];
+  let [number, setNumber] = useState([10,20,30]);
 
 let a =   prices.map((a,i) => {
       return 10
@@ -13,12 +16,36 @@ let a =   prices.map((a,i) => {
   return (
       <div>
           <h4 className = "title" >상품 목록</h4>
+
+          {/* <span>{number[0]}</span>
+          <button onClick = {() => {
+            let copy = [...number];
+            copy[0]++;
+            setNumber(copy);
+          }}>+</button>
+          <span>{number[1]}</span>
+          <button>+</button>
+          <span>{number[2]}</span>
+          <button>+</button> */}
+          
         {
           goods.map((a,i) => {
           return (
             <div className = "food" key = {i}>
               <img src = {'/food' + i + '.png'} className = "food-img" />
               <h4> {a} {prices[i]} </h4>
+              <span>{number[i]}</span>
+              <button style={{margin : "10px"}} onClick = {() => { 
+                let copy = [...number];
+                copy[i]++;
+                setNumber(copy);
+              }}>+</button>
+
+              <button style={{margin : '10px'}}  onClick = {() => { 
+                let copy2 = [...number];
+                copy2[i]--;
+                setNumber(copy2);
+              }}>-</button>
             </div>
                 )
           })
